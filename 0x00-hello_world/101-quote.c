cab3a0e2dea5af7a4d61d083729e38c648f52957
precawise <udofiaprecawise@gmail.com>
2023-02-17 02:56:20 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
 * main - entry point
 *
-* Return: 0
+* Return: 1
 */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19,\n", 59);
-	return (0);
+	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19,\n", 59);
+	return (1);
 }
