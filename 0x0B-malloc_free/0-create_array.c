c3475273b6415b51f40562daf74c273818e0f50d
precawise <udofiaprecawise@gmail.com>
2023-03-15 04:18:12 -0700

    new

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,19 +1,29 @@
-#include "notrebloh.h"
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
-#include <limits.h>
+#include <stdio.h>
 
 /**
- * *malloc_checked - allocate memory with malloc
- * @b: unsigned int type
- * Return: return pointer
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: input size
+ * @c: input character
+ *
+ * Return: a pointer to the array, or NULL if it fails
  */
-void *malloc_checked(unsigned int b)
-{
-	int *ptr;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *arr;
+
+	arr = (char *) malloc(size);
+	if (size == 0 || arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
 }
