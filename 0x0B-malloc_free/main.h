072ceb8cdb8d4116d6e69514b070eeaf68cfcff8
precawise <udofiaprecawise@gmail.com>
2023-03-15 05:59:02 -0700

    create

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main
- *
- * Desc: header file
- */
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -12,6 +10,4 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-char **strtow(char *str);
-
-# endif
+#endif
