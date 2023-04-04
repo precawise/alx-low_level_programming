2775bec9ca1949506f98d8e95bf9c01fa59ce096
precawise <udofiaprecawise@gmail.com>
2023-04-04 07:54:18 -0700

    fixed

---
+++ 0x13-more_singly_linked_lists/0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ * @one: first number input
+ * @two: second number input
+ * Return: 1 + 2
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
---
+++ 0x13-more_singly_linked_lists/0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+c - introductory projects
