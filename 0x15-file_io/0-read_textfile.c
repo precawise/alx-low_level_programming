e86a6310523584be0312cb8bd0c045e8b3d247b0
precawise <udofiaprecawise@gmail.com>
2023-04-07 09:33:53 -0700

    she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,14 @@
-iinclude "main.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to a POSIX stdout.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
- *	function should read and print.
+ *           function should read and print.
  *
- * Return:If the function fails or filename is NULL - 0.
- *	 O/w - the actual number of bytes the function can read	and print.
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -19,25 +19,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	w = writes(STDOUT_FILENO, buffer, r);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(0);
+	close(o);
 
 	return (w);
 }
