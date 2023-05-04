4555945b127dc28233f3100a43831d8828913d10
precawise <udofiaprecawise@gmail.com>
2023-05-04 08:20:21 -0700

    comit again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * binary_to_unit - it convert a binary into an unsigned integer
+ * binary_to_uint - it convert a binary into an unsigned integer
  * @b: it type const char pointer of 1 bit
- * Return: converted number, or zero if 1 or more char in string b that is not 0 or 1
+ * Return: converted number, or 0 if 1 or more char in str b that is not 0 or 1
  */
 
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
 	unsigned int net = 0;
@@ -15,7 +15,7 @@ unsigned int binary_to_unit(const char *b)
 		return (0);
 	while (*(b + n) != '\0')
 	{
-		if (*(b + n) != '0' && * (b + n) != '1')
+		if (*(b + n) != '0' && *(b + n) != '1')
 			return (0);
 		net <<= 1;
 		if (*(b + n) == '1')
