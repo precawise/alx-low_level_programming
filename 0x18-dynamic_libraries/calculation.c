31600fb5304b1de8ebed564018a893137ec82444
precawise <udofiaprecawise@gmail.com>
2023-06-24 07:43:58 -0700

    100-operations

---
+++ 0x18-dynamic_libraries/calculation.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * add - Calculates the remainder of division between two integers.
+ * @a: The first integer (dividend).
+ * @b: The second integer (divisor).
+ * Return: The remainder of the division between the two integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Calculates the remainder of division between two integers.
+ * @a: The first integer (dividend).
+ * @b: The second integer (divisor).
+ * Return: The remainder of the division between the two integers.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - Calculates the remainder of division between two integers.
+ * @a: The first integer (dividend).
+ * @b: The second integer (divisor).
+ * Return: The remainder of the division between the two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Calculates the remainder of division between two integers.
+ * @a: The first integer (dividend).
+ * @b: The second integer (divisor).
+ * Return: The remainder of the division between the two integers.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Calculates the remainder of division between two integers.
+ * @a: The first integer (dividend).
+ * @b: The second integer (divisor).
+ * Return: The remainder of the division between the two integers.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
