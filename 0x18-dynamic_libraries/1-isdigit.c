13437fe7098dc6c70dca4fa0b31da7fa7071276a
precawise <udofiaprecawise@gmail.com>
2023-06-24 05:10:19 -0700

    commmit lib

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
 /**
  * _isdigit - checks for a digit (0 through 9)
- * @c: the character to be checked
- *
+ * @c: int to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
